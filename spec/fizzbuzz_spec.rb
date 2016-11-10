9eaec5f548bede6c946c247cacb33bc6ea8621ac
Veronica Sonzini <mavesonzini@gmail.com>
2016-11-10 09:50:28 +0000

    fizzbuzz

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,19 +1,19 @@
-require './lib/fizzbuzz'
+require 'fizzbuzz'
 
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
+  it "will return 'fizz' when passed 3" do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
 
-  it 'returns "buzz" when passed 5' do
+  it "will return 'buzz' when passed 5" do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
 
-  it 'returns "number" when passed 88' do
-    expect(fizzbuzz(88)).to eq 88
+  it "will return 'fizzbuzz' when passed 15" do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end
 
-  it 'returns "fizzbuzz" when passed 15' do
-    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  it "will return 88 when passed 88" do
+    expect(fizzbuzz(88)).to eq 88
   end
 end
